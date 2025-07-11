dd1bf8d57f8ed143717ab0f41ece63a5e5dfca29
devilsbane98 <tusharkendre7@gmail.com>
2025-07-11 15:15:02 -0400

    fix title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Tushar Kendre - Portfolio',
+  description: 'Resume of Tushar Kendre, a Full Stack Developer',
   generator: 'v0.dev',
 }
 
